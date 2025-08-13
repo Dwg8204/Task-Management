74190ef425b4bee2c6d1233c16782faa18634571
Dwg8204 <duongba0802@gmail.com>
2025-08-13 10:39:12 +0700

    Cài đặt CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
 
@@ -7,7 +8,7 @@ const port = process.env.PORT;
 const database = require('./config/database');
 // create application/json parser
 app.use(bodyParser.json());
-
+app.use(cors());
 const routesApiver1= require('./api/v1/routes/index.route');
 database.connect();
 //Routes version 1
